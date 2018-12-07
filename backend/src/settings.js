ddd00a22d214249b28cd371be90955860a4e2bbd
3zk1m0 <sander.kallio@gmail.com>
2018-12-08 00:33:43 +0200

    stuff

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -1,6 +1,13 @@
 // Define Mysql connection
-export const connectionSettings = {
+export const connectionSettingsOff = {
   host: 'db',
+  user: 'root',
+  database: 'db_1',
+  password: 'db_rootpass',
+  namedPlaceholders: true,
+};
+export const connectionSettings = {
+  host: 'localhost',
   user: 'root',
   database: 'db_1',
   password: 'db_rootpass',
